bd3174a15547b5a0489764ade25598ba64bf2538
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2010-03-04 21:01:43 +0100

    Deleted unneeded models

--- tags/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Tag (models.Model):
-    """ A tag has many snippets """
-    name = models.CharField(max_length=200)
-    def __unicode__(self):
-        return self.name
-    class Meta:
-        ordering = ['name']
