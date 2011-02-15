eda29cec3391fa408aa2bf8d7bf7b53f41bcd5c6
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2011-02-15 14:20:26 +0200

    Added more details to accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from models import UserFollow, UserProfile
 
-admin.site.register(UserFollow)
-admin.site.register(UserProfile)
+class UserFollowAdmin(admin.ModelAdmin):
+    list_display=['user', 'followed_user']
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=['user', 'location', 'url', 'profile_privacy']
+
+admin.site.register(UserFollow, UserFollowAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
